5fd358e817b61f8b08689dae139363031ce0b146
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-03-25 01:07:45 +0530

    gallery1

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-
 const Gallery = () => {
 
   return (
     <div className='w-full m-w-5xl p-2 mx-auto mb-2 gap-2 columns-3'>
-      
-     <div className='img-gallery-container'><img src='/images/DSC_7448.JPG' className='img-container'></img></div> 
-     <div className='img-gallery-container'><img src='/images/DSC_7467.JPG' className='img-container'></img></div> 
-     <div className='img-gallery-container'><img src='/images/DSC_7548.JPG' className='img-container'></img></div> 
-     <div className='img-gallery-container'><img src='/images/DSC_7325.JPG' className='img-container'></img></div> 
-     <div className='img-gallery-container'><img src='/images/DSC_7396.JPG' className='img-container'></img></div> 
-     <div className='img-gallery-container'><img src='/images/DSC_7447.JPG' className='img-container'></img></div> 
+      <h1 className='text-red-600'>kishfish</h1>
+     <div className='img-gallery-container'><img src='/images/DSC_7448.JPG' alt='img' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7467.JPG' alt='img' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7548.JPG' alt='img' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7325.JPG' alt='img' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7396.JPG' alt='img' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7447.JPG' alt='img' className='img-container'></img></div> 
     </div>
   )
 }
