12f9fb9f9e725e0acc112f26668cb1b79f9b998f
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-03-25 00:40:07 +0530

    gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { useState } from 'react';
 
 
 const Gallery = () => {
 
-  const [isHover,setIsHover] = useState(false)
-
   return (
-    <div className='w-full m-w-5xl p-2'>
-      <h1 className='text-[12rem] text-red-600'>hii</h1>
+    <div className='w-full m-w-5xl p-2 mx-auto mb-2 gap-2 columns-3'>
       
      <div className='img-gallery-container'><img src='/images/DSC_7448.JPG' className='img-container'></img></div> 
-      <img src='/images/DSC_7467.JPG'></img>
-      <img src='/images/DSC_7548.JPG'></img>
-      <img src='/images/DSC_7325.JPG'></img>
-      <img src='/images/DSC_7396.JPG'></img>
-      <img src='/images/DSC_7447.JPG'></img>
+     <div className='img-gallery-container'><img src='/images/DSC_7467.JPG' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7548.JPG' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7325.JPG' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7396.JPG' className='img-container'></img></div> 
+     <div className='img-gallery-container'><img src='/images/DSC_7447.JPG' className='img-container'></img></div> 
     </div>
   )
 }
