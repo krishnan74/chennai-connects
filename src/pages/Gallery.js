0c2c79f07ff4cf21f9a41405e3f6e70261d3aa8a
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-03-25 00:30:44 +0530

    img gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+
 
 const Gallery = () => {
+
+  const [isHover,setIsHover] = useState(false)
+
   return (
-    <div>Gallery</div>
+    <div className='w-full m-w-5xl p-2'>
+      <h1 className='text-[12rem] text-red-600'>hii</h1>
+      
+     <div className='img-gallery-container'><img src='/images/DSC_7448.JPG' className='img-container'></img></div> 
+      <img src='/images/DSC_7467.JPG'></img>
+      <img src='/images/DSC_7548.JPG'></img>
+      <img src='/images/DSC_7325.JPG'></img>
+      <img src='/images/DSC_7396.JPG'></img>
+      <img src='/images/DSC_7447.JPG'></img>
+    </div>
   )
 }
 
