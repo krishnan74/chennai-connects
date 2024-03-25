02325f3ef3e0c0ffd03665a3680e79a7cd49f322
krishnan74 <divyakrishnan.r74@gmail.com>
2024-03-25 19:20:54 +0530

    Add hero section and update navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import logo from "../images/logo.jpeg";
+import "../index.css";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between items-center">
+    <div className="w-full flex justify-between items-center px-10 border h-[12vh]">
       <div className="logoDiv">
-        <img src="" alt="" />
+        <img src={logo} alt="" height={120} width={120} />
       </div>
 
-      <div className="navLinks flex justify-evenly items-center">
-        <a href="/" className="navLink">
+      <div className="navLinks flex justify-evenly items-center gap-x-10">
+        <a href="/" className="navLink border-b-2 border-black">
           Home
         </a>
         <a href="/gallery" className="navLink">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Layout from "../layout";
+import backgroundImage from "../images/bg.jpg";
+import "../App.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const styles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "88vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "start",
+    color: "white",
+  };
 
-export default Home
+  return (
+    <Layout>
+      <div className="heroSection" style={styles}>
+        <h1 className="text-[90px] font-bold tracking-wider z-10 mt-20 italic">
+          Race to the <span className=" text-green-400">Future</span> Now
+        </h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
