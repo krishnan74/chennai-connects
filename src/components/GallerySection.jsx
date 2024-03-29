df6e92b1dfe5157bcb88f09dfff5583cbcbd592e
krishnan74 <divyakrishnan.r74@gmail.com>
2024-03-29 10:20:27 +0530

    Refactor CSS styles and update gallery section

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,47 +1,59 @@
-import React from 'react'
-
+import React from "react";
 
 const GallerySection = () => {
   return (
-    <div className='grid-gallery w-full max-w-[900px] mx-auto my-20 gap-1 grid overflow-hidden relative'>
+    <div
+      className={`grid-gallery w-full max-w-[900px] mx-auto my-20 gap-1 grid overflow-hidden relative bg-white`}
+    >
+      <div className="grid-item overflow-hidden">
+        <img src="/images/DSC_1.JPG" alt="img" className="" />
+      </div>
+      <div className="grid-item">
+        <img src="/images/DSC_11.JPG" alt="img" className="" />
+      </div>
+      <div className="grid-item">
+        <img src="/images/DSC_4.JPG" alt="img" className="" />
+      </div>
+      <div className="grid-item">
+        <img src="/images/DSC_5.JPG" alt="img" className="" />
+      </div>
 
-      <div className='grid-item overflow-hidden'>
-        <img src='/images/DSC_1.JPG' alt='img' className='' />
-      </div>
-      <div className='grid-item'>
-        <img src='/images/DSC_11.JPG' alt='img' className='' />
-      </div>
-      <div className='grid-item'>
-        <img src='/images/DSC_4.JPG' alt='img' className='' />
-      </div>
-      <div className='grid-item'>
-        <img src='/images/DSC_5.JPG' alt='img' className='' />
-      </div>
-
-      <div className='grid-item col-span-2 relative'>
+      <div className="grid-item col-span-2 relative">
         <div className="relative">
-          <img src='/images/DSC_6.JPG' alt='img' className='w-full h-full object-cover' />
-          <div className="absolute inset-0 flex items-center justify-center text-white text-[100px] font-bold bg-black bg-opacity-30">CHENNAI CONNECT</div>
+          <img
+            src="/images/DSC_6.JPG"
+            alt="img"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 flex items-center justify-center text-white text-[100px] font-bold bg-black bg-opacity-30">
+            CHENNAI CONNECT
+          </div>
         </div>
       </div>
 
-      <div className='grid-item'>
-        <img src='/images/DSC_7.JPG' alt='img' className='' />
+      <div className="grid-item">
+        <img src="/images/DSC_7.JPG" alt="img" className="" />
       </div>
-      <div className='grid-item'>
-        <img src='/images/DSC_12.JPG' alt='img' className='' />
+      <div className="grid-item">
+        <img src="/images/DSC_12.JPG" alt="img" className="" />
       </div>
 
-    <a href="/Gallery">
-      <div className='grid-item relative'>
-        <div className='relative'>
-          <img src='/images/DSC_2.JPG' alt='img' className='w-full h-full object-cover' />
-          <div className='absolute inset-0 flex items-center justify-center text-white font-serif font-bold bg-black bg-opacity-30'>view more ➤</div>
+      <a href="/Gallery">
+        <div className="grid-item relative">
+          <div className="relative">
+            <img
+              src="/images/DSC_2.JPG"
+              alt="img"
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute tracking-widest text-3xl inset-0 flex items-center justify-center text-white font-bold bg-black bg-opacity-40 font-['Bebas Neue']">
+              View more ➤
+            </div>
+          </div>
         </div>
-      </div>
-</a>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default GallerySection
+export default GallerySection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../layout";
 import backgroundImage from "../images/bg.jpg";
+import fabricImage from "../images/green-fabric.png";
 import "../App.css";
 import Gallery from "./Gallery";
 import GallerySection from "../components/GallerySection";
@@ -10,11 +11,18 @@ const Home = () => {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
-    height: "88vh",
+    height: "70vh",
     display: "flex",
-    justifyContent: "center",
-    alignItems: "start",
+    flexDirection: "column",
+    justifyContent: "start",
+    alignItems: "center",
     color: "white",
+  };
+
+  const galleryStyles = {
+    backgroundImage: `url(${fabricImage})`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
   };
 
   return (
@@ -23,10 +31,26 @@ const Home = () => {
         <h1 className="text-[90px] font-bold tracking-wider z-10 mt-20 italic">
           Race to the <span className=" text-green-400">Future</span> Now
         </h1>
+        <p className="w-[70%] z-10 font-['Oswald'] text-center text-xl tracking-wide ">
+          <span className="text-green-400">Chennai Connects</span> is a dynamic
+          event management startup dedicated to organizing diverse and engaging
+          events in the vibrant city of Chennai. From exhilarating marathons and
+          thrilling sports competitions to lively and fun-filled gatherings, we
+          specialize in creating memorable experiences for participants and
+          spectators alike.
+        </p>
+        <button className="mt-10 button_slide slide_right z-10 hover:text-black px-10 py-3 tracking-wide ">
+          Let's Connect
+        </button>
       </div>
-      <GallerySection/>
+      <div style={galleryStyles}>
+        <h1 className="text-5xl font-bold text-center mt-20">
+          Our <span className="text-green-400">Events</span>
+        </h1>
+        <GallerySection />
+      </div>
     </Layout>
   );
-}
+};
 
 export default Home;
