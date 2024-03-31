7b97e120f22a073b4b2c02c334db69c5eb70a783
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-03-31 15:45:53 +0530

    gallery test 3

--- src/components/PrizeGallery.jsx
+++ src/components/PrizeGallery.jsx
@@ -11,9 +11,7 @@ const PrizeGallery = () => {
     setIndex(index);
   };
 
-
-
-  const prizeGallery = [
+  const lightboxSlides = [
     { src: 'static/images/d1.jpg' },
     { src: 'static/images/d2.jpg' },
     { src: 'static/images/d3.jpg' },
@@ -22,29 +20,51 @@ const PrizeGallery = () => {
     { src: 'static/images/d6.jpg' },
     { src: 'static/images/d7.jpg' },
     { src: 'static/images/d1.jpg' },
-   
   ];
-
-  const lightboxSlides = [...prizeGallery.slice(index), ...prizeGallery.slice(0, index)];
 
   return (
     <div className='w-full p-2'>
-<div className='mx-auto py-3 bg-white gap-[3px] grid grid-cols-2 sm:grid-cols-4 sm:w-[80%] '>
-
-      {prizeGallery.map((item, index) => (
-        <div key={index} onClick={() => handleImageClick(index)} className='grid-item  h-[35vw] sm:h-[20vw]'>
-          <img src={item.src} alt={`img-${index}`} className='w-full h-full object-cover' />
+      <div className='mx-auto py-3 bg-white gap-[3px] grid grid-cols-2 sm:grid-cols-4 sm:w-[80%] '>
+        {/* Image 1 */}
+        <div onClick={() => handleImageClick(0)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d1.jpg' alt='img-0' className='w-full h-full object-cover' />
         </div>
-      ))}
+        {/* Image 2 */}
+        <div onClick={() => handleImageClick(1)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d2.jpg' alt='img-1' className='w-full h-full object-cover' />
+        </div>
+        {/* Image 3 */}
+        <div onClick={() => handleImageClick(2)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d3.jpg' alt='img-2' className='w-full h-full object-cover' />
+        </div>
+        {/* Image 4 */}
+        <div onClick={() => handleImageClick(3)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d4.jpg' alt='img-3' className='w-full h-full object-cover' />
+        </div>
+        {/* Image 5 */}
+        <div onClick={() => handleImageClick(4)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d5.jpg' alt='img-4' className='w-full h-full object-cover' />
+        </div>
+        {/* Image 6 */}
+        <div onClick={() => handleImageClick(5)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d6.jpg' alt='img-5' className='w-full h-full object-cover' />
+        </div>
+        {/* Image 7 */}
+        <div onClick={() => handleImageClick(6)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d7.jpg' alt='img-6' className='w-full h-full object-cover' />
+        </div>
+        {/* Image 8 */}
+        <div onClick={() => handleImageClick(7)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src='static/images/d1.jpg' alt='img-7' className='w-full h-full object-cover' />
+        </div>
+      </div>
       <div>
         <Lightbox
           open={open}
           slides={lightboxSlides}
           close={() => setOpen(false)}
-        
         />
       </div>
-    </div>
     </div>
   );
 };
