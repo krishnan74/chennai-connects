064cfa52390815906158c1e8cd09b2119fee4ca2
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-03-31 16:24:35 +0530

    gallery test 4

--- src/components/PrizeGallery.jsx
+++ src/components/PrizeGallery.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Lightbox from 'yet-another-react-lightbox';
 import "yet-another-react-lightbox/styles.css";
 
@@ -12,40 +11,40 @@ const PrizeGallery = () => {
     setIndex(index);
   };
 
+
+
   const prizeGallery = [
-    { src: 'static/images/d1.jpg', alt: 'img-1', height: '100%', width: '100%' },
-    { src: 'static/images/d2.jpg', alt: 'img-2', height: '100%', width: '100%' },
-    { src: 'static/images/d3.jpg', alt: 'img-3', height: '100%', width: '100%' },
-    { src: 'static/images/d4.jpg', alt: 'img-4', height: '100%', width: '100%' },
-    { src: 'static/images/d5.jpg', alt: 'img-5', height: '100%', width: '100%' },
-    { src: 'static/images/d6.jpg', alt: 'img-6', height: '100%', width: '100%' },
-    { src: 'static/images/d7.jpg', alt: 'img-7', height: '100%', width: '100%' },
-    { src: 'static/images/d1.jpg', alt: 'img-8', height: '100%', width: '100%' },
+    { src: 'static/images/d1.jpg' },
+    { src: 'static/images/d2.jpg' },
+    { src: 'static/images/d3.jpg' },
+    { src: 'static/images/d4.jpg' },
+    { src: 'static/images/d5.jpg' },
+    { src: 'static/images/d6.jpg' },
+    { src: 'static/images/d7.jpg' },
+    { src: 'static/images/d1.jpg' },
+   
   ];
 
   const lightboxSlides = [...prizeGallery.slice(index), ...prizeGallery.slice(0, index)];
 
   return (
     <div className='w-full p-2'>
-      <div className='mx-auto py-3 bg-white gap-[3px] grid grid-cols-2 sm:grid-cols-4 sm:w-[80%] '>
-        {prizeGallery.map((item, idx) => (
-          <div key={idx} onClick={() => handleImageClick(idx)} className='grid-item h-[35vw] sm:h-[20vw]'>
-            <LazyLoadImage
-              alt={item.alt}
-              src={item.src}
-              height={item.height}
-              width={item.width}
-              className='w-full h-full object-cover' />
-          </div>
-        ))}
-        <div>
-          <Lightbox
-            open={open}
-            slides={lightboxSlides}
-            close={() => setOpen(false)}
-          />
+<div className='mx-auto py-3 bg-white gap-[3px] grid grid-cols-2 sm:grid-cols-4 sm:w-[80%] '>
+
+      {prizeGallery.map((item, index) => (
+        <div key={index} onClick={() => handleImageClick(index)} className='grid-item  h-[35vw] sm:h-[20vw]'>
+          <img src={item.src} alt={`img-${index}`} loading='lazy' className='w-full h-full object-cover' />
         </div>
+      ))}
+      <div>
+        <Lightbox
+          open={open}
+          slides={lightboxSlides}
+          close={() => setOpen(false)}
+        
+        />
       </div>
+    </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -55,7 +55,6 @@ const Home = () => {
           <GallerySection />
         </div>
       </div>
-      <PrizeGallery/>
     </Layout>
   );
 };
