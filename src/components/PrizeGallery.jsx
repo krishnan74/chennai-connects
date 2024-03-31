39c4b7a16b6350abb43f9350d98d7daa957caa16
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-03-31 16:31:54 +0530

    jpg test

--- src/components/PrizeGallery.jsx
+++ src/components/PrizeGallery.jsx
@@ -14,14 +14,14 @@ const PrizeGallery = () => {
 
 
   const prizeGallery = [
-    { src: 'static/images/d1.jpg' },
-    { src: 'static/images/d2.jpg' },
-    { src: 'static/images/d3.jpg' },
-    { src: 'static/images/d4.jpg' },
-    { src: 'static/images/d5.jpg' },
-    { src: 'static/images/d6.jpg' },
-    { src: 'static/images/d7.jpg' },
-    { src: 'static/images/d1.jpg' },
+    { src: 'static/images/d1.JPG' },
+    { src: 'static/images/d2.JPG' },
+    { src: 'static/images/d3.JPG' },
+    { src: 'static/images/d4.JPG' },
+    { src: 'static/images/d5.JPG' },
+    { src: 'static/images/d6.JPG' },
+    { src: 'static/images/d7.JPG' },
+    { src: 'static/images/d1.JPG' },
    
   ];
 
